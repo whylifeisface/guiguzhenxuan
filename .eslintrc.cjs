95e8329c700cd4d367c179e0a3ccc474421b1cbe
soup <o826y396@163.com>
2023-05-31 18:00:10 +0800

    fix: one

---
+++ .eslintrc.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    parser: "@typescript-eslint/parser",
+    jsxPragma: "React",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    //eslint(https://eslint.bootcss.com/doc/rules)
+    "no-var": "error",
+    "no-multiple-empty-lines": ["warn", { max: 1 }],
+    "no-console": process.env.Node_ENV == "production" ? "error" : "off",
+    "no-debugger": process.env.Node_ENV == "production" ? "error" : "off",
+    "no-unexpect-multiline": "error",
+    "no-useless-escape": "off",
+    //typeScript(https://typescript-eslint.io/rules)
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/prefer-ts-expect-error": "error",
+    "@typescript-eslint/no-expliciit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/semi": "off",
+    //eslint-plugin-vue(https://eslint.vuejs.org/rules/)
+    "vue/multi-word-component-names": "off",
+    "vue/script-setup-uses-vars": "error",
+    "vue/no-mutating-props": "off",
+    "vue/attribiute-hyphenation": "off",
+  },
+};
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "./style.css";
+import ElementPlus from "element-plus";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
+
+// const a = 100;
