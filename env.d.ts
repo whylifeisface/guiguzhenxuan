026ea763b2116a8c583bf4c1c4853fbbd30b6aa2
soup <o826y396@163.com>
2023-06-04 18:42:42 +0800

    fix: git bug 2

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
-import {DefineComponent} from "vue"
+// import {DefineComponent} from "vue"
 
-declare module "*.vue" {
-    const component:DefineComponent<{},{},any}>
-    export default component
-}
+// declare module "*.vue" {
+//     const component:DefineComponent<{},{},any}>
+//     export default component
+// }
 //vite 使用的是ts ts 不识别.vue后缀的文件
